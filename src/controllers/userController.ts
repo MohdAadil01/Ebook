b6f5170b5d5c8ef154731555aedc64b2001ccb4f
MohdAadil01 <mohdaadil4240@gmail.com>
2024-04-21 15:13:23 +0530

    Hashed password

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
+import User from "../models/User";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
+
+  // VALIDATION
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required");
     return next(error);
   }
+
+  const user = User.findOne({ email });
+  if (!user) {
+    const error = createHttpError("User already exists. Please Login");
+    return next(error);
+  }
+
+  // ENCRYPT PASSWORD
+  const hashedPassword = await bcrypt.hash(password, 8);
   res.json({ message: "Created" });
 };
 
