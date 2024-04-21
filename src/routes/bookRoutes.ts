b44a2eba26e2c8c1b07b3cc15963f1e34a4f3c37
MohdAadil01 <mohdaadil4240@gmail.com>
2024-04-21 22:26:42 +0530

    add multer middleware

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,8 +1,22 @@
 import express from "express";
 import { createBook } from "../controllers/bookController";
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
 
-router.post("/", createBook);
+const upload = multer({
+  dest: path.resolve(__dirname + "../../public/data/uploads"),
+  limits: { fileSize: 3e7 },
+});
+
+router.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
+);
 
 export default router;
