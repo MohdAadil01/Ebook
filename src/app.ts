8ac16b8e7c348a8afe6b85e9bf4e178235eb94ac
MohdAadil01 <mohdaadil4240@gmail.com>
2024-04-21 12:27:36 +0530

    Add Global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import createHttpError from "http-errors";
 
 const app = express();
 
 app.get("/", (req, res) => {
+  const err = createHttpError("error");
+  throw err;
   res.json({ message: "First Route" });
 });
 
+// GLOBAL ERROR HANDLER
+app.use(globalErrorHandler);
 export default app;
