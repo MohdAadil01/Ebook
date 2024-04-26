30cdd193c4ffa0d8141c33c20ee41911d77ce52a
MohdAadil01 <mohdaadil4240@gmail.com>
2024-04-26 14:41:04 +0530

    Add cors configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRoutes from "./routes/userRoutes";
 import bookRoutes from "./routes/bookRoutes";
+import { config } from "./config/config";
 const app = express();
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
